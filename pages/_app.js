5799306dce614d4cc4fb4731a3ae40e7fe60bbb8
vihar <vihar.kurama@gmail.com>
2020-12-27 15:51:37 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.scss";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
