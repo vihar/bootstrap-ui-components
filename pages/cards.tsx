241a758c4279fafe2528d7a7a4a5ee349f8884c4
vihar <vihar.kurama@gmail.com>
2020-12-30 22:26:48 +0530

    feature blog cards

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -1,7 +1,11 @@
 // layout
 import { ComponentLayout } from "layouts";
 // components
-import { SimpleCards, BorderLessCards } from "components/blogsections";
+import {
+  SimpleCards,
+  BorderLessCards,
+  BlogFeatureSection,
+} from "components/blogsections";
 
 function CardsComponents() {
   return (
@@ -16,6 +20,12 @@ function CardsComponents() {
         <div className="mb-5">
           <div className="border">
             <BorderLessCards />
+          </div>
+        </div>
+
+        <div className="mb-5">
+          <div className="border">
+            <BlogFeatureSection />
           </div>
         </div>
       </div>
