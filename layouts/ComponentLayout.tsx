5579cbd251316f65d7c85118f0f4164b13c297a0
vihar <vihar.kurama@gmail.com>
2021-01-19 12:20:12 +0530

    component-layout

--- layouts/ComponentLayout.tsx
+++ layouts/ComponentLayout.tsx
@@ -11,36 +11,43 @@ interface IComponentLayout {
 const ComponentLayout: FC<IComponentLayout> = (props) => {
   const { title, view, doc, code, preview } = props;
   return (
-    <div>
-      {title}
-      <ul className="nav nav-pills" id="myTab" role="tablist">
-        <li className="nav-item" role="presentation">
-          <a
-            className="nav-link active"
-            id="code-tab"
-            data-bs-toggle="tab"
-            href={"#" + code}
-            role="tab"
-            aria-controls="code"
-            aria-selected="true"
-          >
-            View
-          </a>
-        </li>
-        <li className="nav-item" role="presentation">
-          <a
-            className="nav-link"
-            id="preview-tab"
-            data-bs-toggle="tab"
-            href={"#" + preview}
-            role="tab"
-            aria-controls="preview"
-            aria-selected="false"
-          >
-            Code
-          </a>
-        </li>
-      </ul>
+    <div className="border">
+      <div className="d-flex align-items-center border border-top-0 border-left-0 border-right-0 w-100 flex-row">
+        <div className="flex-grow-1 p-2">
+          <p className="lead mb-0">{title}</p>
+        </div>
+        <div className="p-2">
+          <ul className="nav nav-pills" id="myTab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link active"
+                id="code-tab"
+                data-bs-toggle="tab"
+                href={"#" + code}
+                role="tab"
+                aria-controls="code"
+                aria-selected="true"
+              >
+                View
+              </a>
+            </li>
+            <li className="nav-item" role="presentation">
+              <a
+                className="nav-link"
+                id="preview-tab"
+                data-bs-toggle="tab"
+                href={"#" + preview}
+                role="tab"
+                aria-controls="preview"
+                aria-selected="false"
+              >
+                Code
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div className="tab-content" id="myTabContent">
         <div
           className="tab-pane fade show active"
